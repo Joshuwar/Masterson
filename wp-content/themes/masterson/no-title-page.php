7d3c16a990f366c4abaed0575e11bed48a03d34b
Joshua Bradley <josh.u.war@gmail.com>
2011-06-16 08:34:53 +0100

    made nav no-wrap, added no-title-page

---
+++ wp-content/themes/masterson/no-title-page.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+
+<?php
+wp_nav_menu( array(
+	'container' => 'div',
+	'container_id' => 'navContainer',
+	'container_class' => 'jbasewrap',
+	'theme_location' => 'main_menu', 
+	'menu_id' => 'nav'
+) );
+?>
+<div class="jbasewrap content">
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<?php the_content();
+	endwhile;
+	endif; ?>
+</div>
+<div id="scrollAlert"><img src="<?php bloginfo( 'stylesheet_directory' ); ?>/images/pixel-arrow.png" /></div>
+<?php get_footer(); ?>
